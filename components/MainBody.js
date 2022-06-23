436d8cd1907f193f25c15318b80b02ccd502e84f
Ivan Montes <thenavi_prim@hotmail.com>
2022-06-23 16:03:37 -0500

    create home page and components

---
+++ components/MainBody.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import HomeGraphics from "./HomeGraphics";
+import MainButtons from "./MainButtons";
+
+const MainBody = ()=>{
+  return(
+    <View style={ContaninerGraphics.main}>
+      <HomeGraphics></HomeGraphics>
+      <MainButtons></MainButtons>
+    </View>
+  )
+}
+
+const ContaninerGraphics = StyleSheet.create({
+  main: {
+    height: '100%'
+  }
+})
+
+export default MainBody;
